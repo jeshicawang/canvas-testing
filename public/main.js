32736f94188adc8ebfa0ef909136e2f4a6921966
jeshicawang <jeshicawang@gmail.com>
2017-02-07 21:46:35 -0800

    Added margins w/ labeled X & Y axis. #1

--- public/main.js
+++ public/main.js
@@ -9,8 +9,9 @@ const data = [
   {name: "Kwon",     value: 42}
 ];
 
-const width = 960;
-const height = 500;
+const margin = { top: 20, right: 30, bottom: 30, left: 40 }
+const width = 960 - margin.right - margin.left;
+const height = 500 - margin.top - margin.bottom;
 
 const x = d3.scaleBand()
   .domain(data.map(d => d.name))
@@ -20,22 +21,32 @@ const y = d3.scaleLinear()
   .domain([0, d3.max(data, d => d.value)])
   .range([height, 0]);
 
+const xAxis = d3.axisBottom()
+  .scale(x);
+
+const yAxis = d3.axisLeft()
+  .scale(y);
+
 const chart = d3.select('.chart')
-  .attr('width', width)
-  .attr('height', height);
+    .attr('width', width + margin.right + margin.left)
+    .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-const bar = chart.selectAll('g')
+chart.append('g')
+  .attr('class', 'x axis')
+  .attr('transform', 'translate(0,' + height + ')')
+  .call(xAxis)
+
+chart.append('g')
+  .attr('class', 'y axis')
+  .call(yAxis)
+
+chart.selectAll('.bar')
     .data(data)
-  .enter().append('g')
-    .attr('transform', d => 'translate(' + x(d.name) + ',0)');
-
-bar.append('rect')
-  .attr('width', x.bandwidth() - 1)
-  .attr('height', d => height - y(d.value))
-  .attr('y', d => y(d.value))
-
-bar.append('text')
-  .attr('x', x.bandwidth() / 2)
-  .attr('y', d => y(d.value) + 3)
-  .attr('dy', '.75em')
-  .text(d => d.value)
+  .enter().append('rect')
+    .attr('class', 'bar')
+    .attr('x', d => x(d.name))
+    .attr('y', d => y(d.value))
+    .attr('height', d => height - y(d.value))
+    .attr('width', x.bandwidth());
