8b5d4183ce1d5694a280733df0f9c1db81371506
jeshicawang <jeshicawang@gmail.com>
2017-02-07 19:11:07 -0800

    Bar chart WITH svg. #1

--- public/main.js
+++ public/main.js
@@ -2,13 +2,28 @@ const d3 = require('d3');
 
 const data = [4, 8, 15, 16, 23, 42];
 
+const width = 420;
+const barHeight = 20;
+
 const scale = d3.scaleLinear()
   .domain([0, d3.max(data)])
   .range([0, 420]);
 
-d3.select('.chart')
-  .selectAll('div')
+var chart = d3.select('.chart')
+  .attr('width', width)
+  .attr('height', barHeight * data.length);
+
+var bar = chart.selectAll('g')
     .data(data)
-  .enter().append('div')
-    .style('width', (d) => scale(d) + 'px')
-    .text((d) => d)
+  .enter().append('g')
+    .attr('transform', (d, i) => 'translate(0,' + i * barHeight + ')');
+
+bar.append('rect')
+  .attr('width', scale)
+  .attr('height', barHeight - 1);
+
+bar.append('text')
+  .attr('x', d => scale(d) - 3)
+  .attr('y', barHeight / 2)
+  .attr('dy', '.35em')
+  .text(d => d)
