2f26d1d325ac2b96222a25944af5d2d19c406511
jeshicawang <jeshicawang@gmail.com>
2017-02-07 19:28:37 -0800

    Data as an array of object instead of numbers. #1

--- public/main.js
+++ public/main.js
@@ -1,12 +1,19 @@
 const d3 = require('d3');
 
-const data = [4, 8, 15, 16, 23, 42];
+const data = [
+  {name: "Locke",    value:  4},
+  {name: "Reyes",    value:  8},
+  {name: "Ford",     value: 15},
+  {name: "Jarrah",   value: 16},
+  {name: "Shephard", value: 23},
+  {name: "Kwon",     value: 42}
+];
 
 const width = 420;
 const barHeight = 20;
 
 const scale = d3.scaleLinear()
-  .domain([0, d3.max(data)])
+  .domain([0, d3.max(data, d => d.value)])
   .range([0, 420]);
 
 var chart = d3.select('.chart')
@@ -19,11 +26,11 @@ var bar = chart.selectAll('g')
     .attr('transform', (d, i) => 'translate(0,' + i * barHeight + ')');
 
 bar.append('rect')
-  .attr('width', scale)
+  .attr('width', d => scale(d.value))
   .attr('height', barHeight - 1);
 
 bar.append('text')
-  .attr('x', d => scale(d) - 3)
+  .attr('x', d => scale(d.value) - 3)
   .attr('y', barHeight / 2)
   .attr('dy', '.35em')
-  .text(d => d)
+  .text(d => d.value)
