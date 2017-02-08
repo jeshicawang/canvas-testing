70c92d57a61efa6213184ba1cbae79d3bf854595
jeshicawang <jeshicawang@gmail.com>
2017-02-08 10:35:23 -0800

    Alphabet part 1, using index as keys. #2

---
+++ public/main.js
@@ -0,0 +1,36 @@
+const d3 = require('d3');
+
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+const width = 960;
+const height = 500;
+
+const svg = d3.select('.display')
+  .attr('width', width)
+  .attr('height', height)
+
+const g = svg.append('g')
+  .attr('transform', 'translate(32,' + height / 2 + ')');
+
+update(alphabet);
+
+d3.interval(() => {
+  const partAlphabet = d3.shuffle(alphabet).slice(0, Math.floor(Math.random()*26)).sort();
+  update(partAlphabet);
+}, 3000)
+
+function update(data)  {
+  const text = g.selectAll('text')
+    .data(data);
+
+  text.attr('class', 'update');
+
+  text.enter().append('text')
+      .attr('class', 'enter')
+      .attr('x', (d, i) => i * 32)
+      .attr('dy', '.35em')
+    .merge(text)
+      .text(d => d);
+
+  text.exit().remove();
+}
